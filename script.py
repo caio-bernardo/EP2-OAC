b330fc7af9f88165704366957d833fc6138d9ed4
Caio Bernardo <caio.2004vb@gmail.com>
2025-06-13 12:38:25 -0300

    fix: change script to be more generic and increse gen range

--- script.py
+++ script.py
@@ -2,11 +2,14 @@ import random
 
 # Open file for writing
 with open('numbers.txt', 'w') as file:
+    n = 1_000
+    a = -100_000_000.0
+    b = 100_000_000.0
     # Generate 1000 random numbers between 0 and 500,000
-    for _ in range(1000):
+    for _ in range(n):
         # Generate a random float between 0 and 500,000
-        number = random.uniform(0, 5000)
+        number = random.uniform(a, b)
         # Write the number to the file followed by a newline
         file.write(f"{number}\n")
 
-print("File has been created with 1000 random numbers between 0 and 500,000.")
+print(f"File has been created with {n} random numbers between {a} and {b}")
